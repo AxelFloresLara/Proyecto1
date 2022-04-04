4fefda43422339b3fdeb37244e38ad5e03614138
AxelFloresLara <axelsentado@gmail.com>
2022-04-04 00:02:09 -0600

    Una vez concluida la conexión del programa, se inicia a crear la interfaz del cliente.

--- src/sockets/cliente/Cliente.java
+++ src/sockets/cliente/Cliente.java
@@ -1,16 +1,27 @@
 package sockets.cliente;
-import java.io.IOException;
+import java.io.*;
+import sockets.conexion.Conexion;
+import javax.swing.*;
 
-public class Cliente {
-    
-    
-    
-    
-    
-    
+
+public class Cliente extends Conexion{
+    /* Objetos */
+        public JFrame ventana_cliente = null; 
+       
     public Cliente() throws IOException{super("cliente");}
     public void hacer_interfaz(){
         
+        /* Creación de los objetos*/
+        /* JFrame: Es utilizado para crear una nueva ventana, que funcionará posteriormente como contenedor del resto de la interfaz*/
+        
+        ventana_cliente = new JFrame("Cliente");
+        ventana_cliente.setSize(600, 540);
+        ventana_cliente.setVisible(true);
+        ventana_cliente.setLocation(300, 100);
+        ventana_cliente.setResizable(false);
+        ventana_cliente.setLayout(null);
+        ventana_cliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
     }
-          
 }
